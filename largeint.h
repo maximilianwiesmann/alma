ba96ca9d31d20bacf51d15ccf7ce8304300967b1
jonaslippert <44497554+jonaslippert@users.noreply.github.com>
2018-11-15 15:07:42 +0100

    Add files via upload

--- largeint.h
+++ largeint.h
@@ -13,8 +13,15 @@ public:
     LargeInt(inputtype);                           // constructor
     std::string decimal() const;                   // decimal representation
     bool operator<(const LargeInt &) const;        // comparison
+
+    bool is_even() const;                          // Zahl gerade?
+    void div2();                                   // Division durch 2
+    bool is_zero() const;                          // Zahl gleich 0?
+
     LargeInt operator+(const LargeInt &) const;    // addition
     const LargeInt & operator+=(const LargeInt &); // addition
+    LargeInt operator-(const LargeInt &) const;    // subtraction b-a nur gültig für a<=b
+    const LargeInt & operator-=(const LargeInt &); // subtraction b-a nur gültig für a<=b
     LargeInt operator*(const LargeInt &) const;    // multiplication
     const LargeInt & operator*=(const LargeInt &); // multiplication
 
