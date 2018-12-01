300d197395f9e744e4611bb169b6cbf8a6e853ae
jonaslippert <44497554+jonaslippert@users.noreply.github.com>
2018-12-01 13:38:06 +0100

    Abgabe 08

---
+++ PolynomTest.cpp
@@ -0,0 +1,20 @@
+// Test program for Polynomial.h
+
+//
+// Created by Jonas Lippert and Maximilian Wiesmann
+//
+
+#include <iostream>
+#include "Polynomial.h"
+
+int main() {
+    Polynomial P;
+    P.SetCoefficient(7, 1);
+    P.SetCoefficient(3, 6);
+    P.SetCoefficient(0,-1);
+    std::cout << "     P = " <<        P.toString() << "\n";
+    std::cout << "-1 * P = " << (-1 * P).toString() << "\n";
+    std::cout << " 5 * P = " << ( 5 * P).toString() << "\n";
+    std::cout << "-5 * P = " << (-5 * P).toString() << "\n";
+    std::cout << " 0 * P = " << ( 0 * P).toString() << "\n";
+}
